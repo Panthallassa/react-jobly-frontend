637a714ed0e03a12547ab47886586acce72f4ba9
Ana Grieser <anag@Anas-MacBook-Pro.local>
2024-12-12 18:00:12 -0700

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,28 @@ import react from "@vitejs/plugin-react";
 import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
-export default defineConfig({
-	optimizeDeps: {
-		include: ["jwt-decode"],
-	},
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": "http://localhost:3001",
+export default defineConfig(({ mode }) => {
+	return {
+		define: {
+			// Explicitly set NODE_ENV to match the build mode
+			"process.env.NODE_ENV": JSON.stringify(
+				mode === "production" ? "production" : "development"
+			),
 		},
-	},
-	test: {
-		globals: true,
-		environment: "jsdom", // Sets the test environment to jsdom
-		css: true, // Ensures that CSS imports don’t break tests
-		exclude: [...configDefaults.exclude],
-	},
+		optimizeDeps: {
+			include: ["jwt-decode"],
+		},
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/api": "http://localhost:3001", // Proxy backend API requests
+			},
+		},
+		test: {
+			globals: true,
+			environment: "jsdom", // Sets the test environment to jsdom
+			css: true, // Ensures that CSS imports don’t break tests
+			exclude: [...configDefaults.exclude],
+		},
+	};
 });
