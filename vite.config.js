16c05eb42487d003e898bf61f22f61db620f4bfc
Ana Grieser <anag@Anas-MacBook-Pro.local>
2024-12-16 20:51:00 -0700

    debugging signup

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,21 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	define: {
-		// Use REACT_APP_BASE_URL from the environment or default to localhost
+		// Use VITE_BASE_URL from the environment or default to localhost
 		"process.env.BASE_URL": JSON.stringify(
-			process.env.REACT_APP_BASE_URL ||
+			process.env.meta.VITE_BASE_URL ||
 				"http://localhost:3001"
 		),
 	},
-	server: {
-		// Proxy API calls to the backend
-		proxy: {
-			"/api": {
-				target:
-					process.env.REACT_APP_BASE_URL ||
-					"http://localhost:3001",
-				changeOrigin: true, // Ensure the origin header matches the target
-			},
-		},
-	},
+	// server: {
+	// 	// Proxy API calls to the backend
+	// 	proxy: {
+	// 		"/api": {
+	// 			target:
+	// 				process.env.REACT_APP_BASE_URL ||
+	// 				"http://localhost:3001",
+	// 			changeOrigin: true, // Ensure the origin header matches the target
+	// 		},
+	// 	},
+	// },
 });
