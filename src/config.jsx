9c7a46e12524362f9583902739577be86e3113ea
Ana Grieser <anag@Anas-MacBook-Pro.local>
2024-12-16 20:01:32 -0700

    debugging

--- src/config.jsx
+++ src/config.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 	baseURL:
 		import.meta.env.VITE_BASE_URL ||
-		"http://localhost:3001/api", // Use environment variable
+		"http://localhost:3001",
 });
 
 export default api;
